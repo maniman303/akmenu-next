695047568738e881b9090cb666132a9c02323f00
maniman303 <maniman303@gmail.com>
2026-04-12 23:00:13 +0000

    Fix missing ticking sound

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -137,7 +137,7 @@ int main(int argc, char* argv[]) {
     // init graphics
     gdi().init();
 
-    // sd().initArm7RegStatuses();
+    sd().initArm7RegStatuses();
 
     fsManager().init(argc, argv);
 
